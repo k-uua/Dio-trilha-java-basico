c00b4123422a686e13a9d66e3c5ad5fbdf1b2c1e
Kauã <kauadossantossilva02@gmail.com>
2024-06-12 18:44:06 +0000

    Entendendo o funcionamento de incrementação

---
+++ Operadores/src/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+    public static void main(String[] args) {
+    
+    //Mudança de um numero negativo para positivo
+    int numero = 6;
+    numero = - numero;
+    System.out.println(numero);
+
+    numero = numero * -1;
+    System.out.println(numero);
+    
+    }
+}
